865ce65da04f6757dddacb322da6f4c1a502f313
Juan Carlos <juancarlosti@rede.ulbra.br>
2024-09-19 10:57:49 -0300

    codigo aleatorio

--- src/App.js
+++ src/App.js
@@ -23,27 +23,3 @@ function App() {
 }
 
 export default App;
-
-const PontoPisca = keyframes`
-  0% {opacity: 0;}
-  50% {opacity: 1;}
-  100% {opacity: 0;}
-`
-
-const Ponto = styled.span`
-  font-size: 72px;
-  animation: ${PontoPisca} 1.5s infinite;
-  &:nth-child(2){
-    animation-delay: 0.5s;
-  }
-  &:nth-child(3){
-    animation-delay: 1s;
-  }
-`
-
-const P = styled.p`
-  display:flex;
-  justify-content:center;
-  align-items: center;
-`
-
